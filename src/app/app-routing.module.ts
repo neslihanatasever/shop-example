2fc8288d9778b5a3f4a439461bcd5d836c1700ac
Neslihan Atasever <neslihan_atasvr111@outlook.com>
2022-05-26 16:11:00 +0300

    Angular forms2 the end

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import {ProductComponent} from "./product/product.component";
 import {ProductAddForms1Component} from "./product/product-add-forms1/product-add-forms1.component";
 import {ProductAddForms2Component} from "./product/product-add-forms2/product-add-forms2.component";
+import {LoginComponent} from "./login/login.component";
+import {LoginGuard} from "./login/login.guard";
 
 const routes: Routes = [
   {path: 'products', component: ProductComponent},
-  {path: 'product-add-1', component: ProductAddForms1Component},
+  {path: 'product-add-1', component: ProductAddForms1Component, canActivate:[LoginGuard]},
   {path: 'product-add-2', component: ProductAddForms2Component},
   // {path: 'product-add-2', component: ProductComponent},
   {path:'', redirectTo: 'products', pathMatch: 'full'},
-  {path:'products/category/:categoryId', component: ProductComponent}
+  {path:'products/category/:categoryId', component: ProductComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,9 @@ import {HttpClientModule} from "@angular/common/http";
 import {CategoryService} from "./services/category.service";
 import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
 import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
+import { LoginComponent } from './login/login.component';
+import {AccountService} from "./services/account.service";
+import {LoginGuard} from "./login/login.guard";
 
 
 @NgModule({
@@ -23,7 +26,8 @@ import { ProductAddForms2Component } from './product/product-add-forms2/product-
     ProductComponent,
     ProductFilterPipe,
     ProductAddForms1Component,
-    ProductAddForms2Component
+    ProductAddForms2Component,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,7 @@ import { ProductAddForms2Component } from './product/product-add-forms2/product-
     HttpClientModule,
     ReactiveFormsModule
   ],
-  providers: [AlertifyService, CategoryService],
+  providers: [AlertifyService, CategoryService, AccountService, LoginGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
